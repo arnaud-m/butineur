ab0e1ad7f8025ee0002dc13b2712bc4098a71bcc
Arnaud Malapert <arnaud.malapert@unice.fr>
2017-09-25 00:06:04 +0200

    remove unused discipline filter and document plot functions

--- server.R
+++ server.R
@@ -32,13 +32,21 @@ FilterDisciplines <- function(dataMin) {
 
 dataMinM <- ReadMinIP(file = file.path("data", "fr-esr-insertion_professionnelle-master.csv"))
 dataMinDomM <- FilterDomains(dataMinM)
-dataMinDiscM <- FilterDisciplines(dataMinM)
+## TODO dataMinDiscM <- FilterDisciplines(dataMinM)
 
 dataMinLP <- ReadMinIP(file = file.path("data", "fr-esr-insertion_professionnelle-lp.csv"))
 dataMinDomLP <- FilterDomains(dataMinLP)
-dataMinDiscLP <- FilterDisciplines(dataMinLP)
+## TODO dataMinDiscLP <- FilterDisciplines(dataMinLP)
 
-## Compute percentage labels of bar plots
+##' Compute labels for percentages of a vector
+##'
+##' Generate labels for percentages that are greater than the minimal threshold.
+##' @title 
+##' @param x the percentage vector
+##' @param threshold the minimal threshold to label a percentage
+##' @param digits the number of digits of the percentage labels 
+##' @return a vector of labels (character)
+##' @author Arnaud Malapert
 GetPercentLabels <- function(x, threshold = 1, digits = 1) {
   ind <- x >= threshold
   r <- rep("", length(x))
@@ -46,18 +54,40 @@ GetPercentLabels <- function(x, threshold = 1, digits = 1) {
   return(r)
 }
 
-## http://stackoverflow.com/questions/21236229/stacked-bar-chart
-## http://rstudio-pubs-static.s3.amazonaws.com/4305_8df3611f69fa48c2ba6bbca9a8367895.html
-## http://www.sthda.com/french/wiki/ggplot2-barplots-guide-de-demarrage-rapide-logiciel-r-et-visualisation-de-donnees
+
+
+##' Generate a simple bar chart computing the percentages of all values in the vector `x`.
+##'
+##' The bar chart has x-axis labels if if their percentages are greater than the minimal threshold.
+##' @title 
+##' @param x the vector
+##' @param threshold the minimal threshold to label a bar 
+##' @param digits the number of digits of the percentage labels
+##' @return a ggplot object
+##' @references \url{http://rstudio-pubs-static.s3.amazonaws.com/4305_8df3611f69fa48c2ba6bbca9a8367895.html}, \url{http://www.sthda.com/french/wiki/ggplot2-barplots-guide-de-demarrage-rapide-logiciel-r-et-visualisation-de-donnees}
+##' @author Arnaud Malapert
 BarPlotRaw <- function(x, threshold = 1, digits = 0) {
   x <- as.data.frame(table(x[drop=TRUE], useNA = "ifany"))
   label <- GetPercentLabels(100*x$Freq/sum(x$Freq), threshold = threshold, digits = digits)
   pos <- x$Freq / 2
-  ggplot(x, aes(x = Var1, y = Freq)) + geom_bar(stat="identity", position="dodge", fill = ptol_pal()(1)) +
+  ggplot(x, aes(x = Var1, y = Freq)) + geom_bar(stat = "identity", position = "dodge", fill = ptol_pal()(1)) +
     geom_text(aes(y = pos, label=label), color = "white", size=8, fontface = 2) +
     coord_flip() + theme_gdocs() 
 }
 
+##' Generate a stacked bar plot computing the percentages of all pairs in the columns `aesX` and `aesY` of the data `df`.
+##'
+##' The stacked bar chart has optional labels for the x-axis and fill categories.
+##' @title 
+##' @param df the data with columns aesX and aesF
+##' @param aesX x-axis of the plot 
+##' @param aesF categories of the stacked bars
+##' @param legend.title title of the legend
+##' @param labelX `TRUE` indicates that each stacked bar is labeled 
+##' @param labelF `TRUE` indicates that each bar is labeled 
+##' @return a ggplot object
+##' @references \url{http://stackoverflow.com/questions/21236229/stacked-bar-chart}
+##' @author Arnaud Malapert
 BarStackedPlotRaw <- function(df, aesX, aesF, legend.title = NULL, labelX = TRUE, labelF = TRUE) {
   x <- as.data.frame(ftable(df[ , c(aesX, aesF), drop=TRUE], exclude= NaN))
   totFreq <- sum(x$Freq)
@@ -68,7 +98,8 @@ BarStackedPlotRaw <- function(df, aesX, aesF, legend.title = NULL, labelX = TRUE
   x$toplab <-  GetPercentLabels(100 * x$top / totFreq, threshold = 1, digits = 0)
   m <- length(unique(x[,aesF]))
   ## exploit recycling
-  x$toplab[ append(rep(TRUE,m-1), FALSE) ] <- "" 
+  x$toplab[ append(rep(TRUE,m-1), FALSE) ] <- ""
+  ## browser()
   p <- ggplot(x, aes_string(x = aesX, y = "Freq", fill = aesF)) + geom_bar(stat="identity") + coord_flip() + theme_gdocs()
   
   if(labelF) {
