ce954f8ceff42fc32df2a832d32bd77af4ccdcb3
Arnaud Malapert <arnaud.malapert@unice.fr>
2017-11-09 16:49:43 +0100

    reactive diploma selection #12

--- server.R
+++ server.R
@@ -41,20 +41,16 @@ dataMinDomLP <- FilterDomains(dataMinLP)
 ## TODO dataMinDiscLP <- FilterDisciplines(dataMinLP)
 
 
-
-
-MakeChoiceLists<- function(data) {
-  su <- function(x) sort(unique(x))
+MakeCoreChoices <- function(data) {
   list(
-    annee=su(data$annee),
-    grade=su(data$libdip1),
-    diplome=list("Domaine"= su(data$libdom), "Mention" = su(data$libdip2), "Spécialité" = su(data$libdip3), "Code SISE" = su(data$code_diplome))
+    annee=sort(unique(data$annee)),
+    grade=levels(data$libdip1)
   )
 }
 
-choices <- MakeChoiceLists(data)
 
-MakeSelectionOutput <- function(input, output, choices) {
+
+MakeCoreSelectionOutput <- function(output, choices) {
   output$checkboxAnnee <- renderUI( {
     checkboxGroupInput("annee", "Année(s)", choices$annee, choices$annee)
   })
@@ -62,20 +58,12 @@ MakeSelectionOutput <- function(input, output, choices) {
   output$checkboxGrade <- renderUI( {
     checkboxGroupInput("grade", "Grade(s)", choices$grade, choices$grade)
   })
-    
-  output$selectizeDiplome <- renderUI( {
-    selectizeInput(
-      'diplome', 'Sélectionner un ou plusieurs domaines, mentions, spécialités ou codes SISE : ', choices$diplome, selected = NULL, multiple = TRUE, 
-      options = list(
-        placeholder = "Taper la sélection ici."
-      ), width = "800px"
-    )
-  })
 }
+
 
 ## https://stackoverflow.com/questions/38653903/r-shiny-repetitive-evaluation-of-the-reactive-expression
 ## https://shiny.rstudio.com/articles/action-buttons.html
-MakeReactiveData <- function(input, data, choices) {
+MakeReactiveCoreData <- function(input, data, choices) {
   reactive({
     logInd <- rep(TRUE, nrow(data))
     if( length(input$annee) < length(choices$annee) ) {
@@ -86,11 +74,44 @@ MakeReactiveData <- function(input, data, choices) {
       ## Sélection active : certains grades ne sont pas sélectionnés.
       logInd <- logInd & (data$libdip1 %in% input$grade)
       }
+    subset(data, logInd)
+  })
+}
+
+
+MakeReactiveSelectizeOutput <- function(input, output, coreData) {
+  output$selectizeDiplome <- renderUI( {
+    data <- coreData()
+    diplomes = list(
+      "Domaine"= levels(data$libdom[drop=TRUE]),
+      "Mention" = levels(data$libdip2[drop=TRUE]),
+      "Spécialité" = levels(data$libdip3[drop=TRUE]),
+      "Code SISE" = sort(unique(data$code_diplome))
+    )
+    selectizeInput(
+      'diplome', 'Sélectionner un ou plusieurs domaines, mentions, spécialités ou codes SISE : ',
+      diplomes, selected = input$diplome, multiple = TRUE, 
+      options = list(
+        placeholder = "Taper la sélection ici."
+      ), width = "800px"
+    )
+  })
+}
+
+
+## https://stackoverflow.com/questions/38653903/r-shiny-repetitive-evaluation-of-the-reactive-expression
+## https://shiny.rstudio.com/articles/action-buttons.html
+MakeReactiveData <- function(input, coreData) {
+  reactive({
+    data <- coreData()
+    logInd <- rep(TRUE, nrow(data))
     if( length(input$sexe) < 2) {
       ## Sélection active : un seul genre est sélectionné.
       logInd <- logInd & (data$sexe %in% input$sexe)
     }
+    
     if(! is.null(input$diplome) ) {
+      ## Sélection active : un sous-ensemble de diplômes est sélectionné.
       logInd <- logInd & (data$libdom %in% input$diplome | data$libdip2 %in% input$diplome | data$libdip3 %in% input$diplome | data$code_diplome %in% input$diplome)
     }
     subset(data, logInd)
@@ -322,12 +343,16 @@ function(input, output, session) {
   ## ######################
   ## Page: Brut 
 
-  MakeSelectionOutput(input, output, choices)
+  coreChoices <- MakeCoreChoices(data)
+  MakeCoreSelectionOutput(output, coreChoices)
 
+  rcoreData <- MakeReactiveCoreData(input, data, coreChoices)
+
+  MakeReactiveSelectizeOutput(input, output, rcoreData)
 
   ## #######################
   ## Ensemble des diplômés
-  rdiplomes <- MakeReactiveData(input, data, choices)
+  rdiplomes <- MakeReactiveData(input, rcoreData)
   
   MakeResultatsOutput(output, rdiplomes)
   
